389e248b2867a5b28ff22b1f92b5f5d1871bed3a
Kelvin R. <sariok@zoho.com>
2025-09-02 14:16:55 -0400

    added simple letter frequency counting

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
+void readDaFile(string fileName);
+
+void readDaFile(string fileName) {
+    vector<int> charFreqs(26);
+    ifstream infile(fileName);
+
+    if (infile) {
+        cout << "It's not fudged! :)" << '\n';
+    }
+
+    else {
+        cout << "It's fudged." << '\n';
+    }
+
+    char ch{};
+    int indexNumber{};
+    // taken from Roberts Programming Abstractions in C++ (2014), pg. 205.
+    while (infile.get(ch)) {
+        if (isalpha(ch)) {
+            indexNumber = toupper(ch) - 'A';
+            charFreqs[indexNumber]++;
+        }
+    }
+
+    for(ch = 'A'; ch <= 'Z'; ch++ ) {
+        cout << ch << ": " << charFreqs[ch - 'A'] << '\n';
+    }
+
+}
+
+int main() {
+    string fileyname = "ciphertext.txt";
+    readDaFile(fileyname);
+    char check;
+
+    cin >> check;
+
     return 0;
 }
